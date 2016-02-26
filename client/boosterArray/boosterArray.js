79ea8c10a5eadb48958a07538ceb70d6c5c83cc7
Ward Gubbi <ward.gubbi@23seconds.be>
2016-02-26 18:21:10 +0000

    Write boosterarray helpers in a more generic way Closes #3

--- client/boosterArray/boosterArray.js
+++ client/boosterArray/boosterArray.js
@@ -3,49 +3,9 @@ Template.boosterArray.onCreated(() => {
 });
 
 Template.boosterArray.helpers({
-	cursorBooster: function() {
+	booster(boosterName) {
 		var numberOfBoosters = [];
-		var booster = Boosters.find({'name': 'Cursor'}).fetch();
-		for(var i = 0; i < booster.length; i++) {
-			for(var j = 0; j < booster[i].count; j++) {
-				numberOfBoosters.push({imgTag: booster[i].imgTag})
-			}
-		}
-		return numberOfBoosters;
-	},
-	grandmaBooster: function() {
-		var numberOfBoosters = [];
-		var booster = Boosters.find({'name': 'Grandma'}).fetch();
-		for(var i = 0; i < booster.length; i++) {
-			for(var j = 0; j < booster[i].count; j++) {
-				numberOfBoosters.push({imgTag: booster[i].imgTag})
-			}
-		}
-		return numberOfBoosters;
-	},
-	farmBooster: function() {
-		var numberOfBoosters = [];
-		var booster = Boosters.find({'name': 'Farm'}).fetch();
-		for(var i = 0; i < booster.length; i++) {
-			for(var j = 0; j < booster[i].count; j++) {
-				numberOfBoosters.push({imgTag: booster[i].imgTag})
-			}
-		}
-		return numberOfBoosters;
-	},
-	mineBooster: function() {
-		var numberOfBoosters = [];
-		var booster = Boosters.find({'name': 'Mine'}).fetch();
-		for(var i = 0; i < booster.length; i++) {
-			for(var j = 0; j < booster[i].count; j++) {
-				numberOfBoosters.push({imgTag: booster[i].imgTag})
-			}
-		}
-		return numberOfBoosters;
-	},
-	bankBooster: function() {
-		var numberOfBoosters = [];
-		var booster = Boosters.find({'name': 'Bank'}).fetch();
+		var booster = Boosters.find({'name': boosterName}).fetch();
 		for(var i = 0; i < booster.length; i++) {
 			for(var j = 0; j < booster[i].count; j++) {
 				numberOfBoosters.push({imgTag: booster[i].imgTag})
