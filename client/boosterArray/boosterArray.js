e3325d262e29e154c618403e019c3c8d0c943b55
Lukas van Driel <lukas@q42.nl>
2016-02-26 21:50:09 +0000

    examples for #1 #2 #3 #4 #5 #6 #7 #8 #9 #10

--- client/boosterArray/boosterArray.js
+++ client/boosterArray/boosterArray.js
@@ -1,54 +1,13 @@
-Meteor.subscribe('boosters');
+Template.boosterArray.onCreated(() => {
+	Meteor.subscribe('boosters');
+});
 
 Template.boosterArray.helpers({
-	cursorBooster: function(event) {
-		var numberOfBoosters = [];
-		var booster = Boosters.find({'name': 'Cursor'}).fetch();
-		for(var i = 0; i < booster.length; i++) {
-			for(var j = 0; j < booster[i].count; j++) {
-				numberOfBoosters.push({imgTag: booster[i].imgTag})
-			}
-		}
-		return numberOfBoosters;
+	boosters() {
+		// TODO order on something
+		return Boosters.find({}, {fields:{name:1, count:1, imgTag:1}});
 	},
-	grandmaBooster: function(event) {
-		var numberOfBoosters = [];
-		var booster = Boosters.find({'name': 'Grandma'}).fetch();
-		for(var i = 0; i < booster.length; i++) {
-			for(var j = 0; j < booster[i].count; j++) {
-				numberOfBoosters.push({imgTag: booster[i].imgTag})
-			}
-		}
-		return numberOfBoosters;
-	},
-	farmBooster: function(event) {
-		var numberOfBoosters = [];
-		var booster = Boosters.find({'name': 'Farm'}).fetch();
-		for(var i = 0; i < booster.length; i++) {
-			for(var j = 0; j < booster[i].count; j++) {
-				numberOfBoosters.push({imgTag: booster[i].imgTag})
-			}
-		}
-		return numberOfBoosters;
-	},
-	mineBooster: function(event) {
-		var numberOfBoosters = [];
-		var booster = Boosters.find({'name': 'Mine'}).fetch();
-		for(var i = 0; i < booster.length; i++) {
-			for(var j = 0; j < booster[i].count; j++) {
-				numberOfBoosters.push({imgTag: booster[i].imgTag})
-			}
-		}
-		return numberOfBoosters;
-	},
-	bankBooster: function(event) {
-		var numberOfBoosters = [];
-		var booster = Boosters.find({'name': 'Bank'}).fetch();
-		for(var i = 0; i < booster.length; i++) {
-			for(var j = 0; j < booster[i].count; j++) {
-				numberOfBoosters.push({imgTag: booster[i].imgTag})
-			}
-		}
-		return numberOfBoosters;
+	count() {
+		return _.times(this.count, () => {});
 	}
 });
